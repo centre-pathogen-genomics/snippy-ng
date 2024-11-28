8910b1cea5b06021a7be2522b35bb6adf4c0cfe7
Wytamma Wirth <wytamma.wirth@me.com>
2024-11-28 14:48:42 +1100

    :sparkles: add command type to commands

--- src/snippy_ng/stages/base.py
+++ src/snippy_ng/stages/base.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
 from abc import abstractmethod
-from typing import List
+from typing import List, Callable
 import subprocess
+import sys
+from io import StringIO
 
 from snippy_ng.logging import logger
 from snippy_ng.dependencies import Dependency
@@ -9,6 +12,14 @@ from pydantic import BaseModel
 
 class BaseOutput(BaseModel):
     pass
+
+class Command(BaseModel):
+    func: Callable
+    args: List = []
+    description: str
+    
+    def __str__(self):
+        return self.description
 
 class BaseStage(BaseModel):
     _dependencies: List[Dependency] = []
@@ -26,21 +37,54 @@ class BaseStage(BaseModel):
 
     @property
     @abstractmethod
-    def commands(self) -> List[str]:
-        """Constructs the shell commands."""
+    def commands(self) -> List[str | Command]:
+        """Constructs the commands."""
         pass
 
     def run(self, quiet=False):
-        """Runs the commands in the shell."""
+        """Runs the commands in the shell or calls the function."""
         for cmd in self.commands:
-            logger.info(f"Running: {cmd}")
+            logger.info(f"Running: {cmd}") 
             stdout = None
             stderr = subprocess.STDOUT
             if quiet:
                 stdout = subprocess.DEVNULL
                 stderr = subprocess.DEVNULL
             try:
-                subprocess.run(cmd, shell=True, check=True, stdout=stdout, stderr=stderr, text=True)
+                if isinstance(cmd, Command):
+                    if quiet:
+                        # Redirect stdout and stderr to StringIO
+                        with StringIO() as out, StringIO() as err, \
+                                self.redirect_output(out), self.redirect_error(err):
+                            cmd.func(*cmd.args)
+                    else:
+                        cmd.func(*cmd.args)
+                elif isinstance(cmd, str):
+                    subprocess.run(cmd, shell=True, check=True, stdout=stdout, stderr=stderr, text=True)
+                else:
+                    raise ValueError(f"Invalid command type: {type(cmd)}")
             except subprocess.CalledProcessError as e:
                 logger.error(f"Command failed with exit code {e.returncode}")
                 raise RuntimeError(f"Failed to run command: {cmd}")
+            except Exception as e:
+                logger.error(f"Function call failed: {e}")
+                raise RuntimeError(f"Failed to run function: {cmd}")
+    
+    @contextmanager
+    def redirect_output(self, output_stream):
+        original_stdout = sys.stdout
+        sys.stdout = output_stream
+        try:
+            yield
+        finally:
+            sys.stdout = original_stdout
+
+    @contextmanager
+    def redirect_error(self, error_stream):
+        original_stderr = sys.stderr
+        sys.stderr = error_stream
+        try:
+            yield
+        finally:
+            sys.stderr = original_stderr
+        
