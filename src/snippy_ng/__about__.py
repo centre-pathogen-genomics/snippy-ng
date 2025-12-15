8a107af16360af0dbd6cdc7da797ca12bdd17e25
semantic-release <semantic-release>
2025-12-15 12:30:13 +0000

    1.14.0
    Automatically generated by python-semantic-release

--- src/snippy_ng/__about__.py
+++ src/snippy_ng/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Wytamma Wirth <wytamma.wirth@me.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "1.13.0"
+__version__ = "1.14.0"
 
 # Version and executable information
 EXE = "snippy-ng"
