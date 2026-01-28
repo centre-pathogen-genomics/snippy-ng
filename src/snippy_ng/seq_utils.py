dac4cd2d4125250e96aa6151761db8c8425f962b
Wytamma Wirth <wytamma.wirth@me.com>
2026-01-28 15:04:07 +1100

    fix: refactor guess_format function for improved readability and structure

--- src/snippy_ng/seq_utils.py
+++ src/snippy_ng/seq_utils.py
@@ -2,25 +2,22 @@ import gzip
 
 
 def guess_format(fname):
+    # Try to open as text, if fails, try gzip
     try:
-        # Try to open as text, if fails, try gzip
-        try:
-            fh = open(fname, 'rt')
-            line = fh.readline()
-            fh.close()
-        except UnicodeDecodeError:
-            fh = gzip.open(fname, 'rt')
-            line = fh.readline()
-            fh.close()
-        if not line:
-            return None
-        if line.startswith("LOCUS"):
-            return 'genbank'
-        elif line.startswith("ID "):
-            return 'embl'
-        elif line.startswith(">"):
-            return 'fasta'
-        else:
-            return None
-    except IOError:
+        fh = open(fname, 'rt')
+        line = fh.readline()
+        fh.close()
+    except UnicodeDecodeError:
+        fh = gzip.open(fname, 'rt')
+        line = fh.readline()
+        fh.close()
+    if not line:
+        return None
+    if line.startswith("LOCUS"):
+        return 'genbank'
+    elif line.startswith("ID "):
+        return 'embl'
+    elif line.startswith(">"):
+        return 'fasta'
+    else:
         return None
