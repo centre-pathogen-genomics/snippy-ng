77731551c9dca709c38abf64638e60cb242e0398
semantic-release <semantic-release>
2026-02-16 04:57:41 +0000

    1.19.0
    Automatically generated by python-semantic-release

--- src/snippy_ng/__about__.py
+++ src/snippy_ng/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Wytamma Wirth <wytamma.wirth@me.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "1.18.0"
+__version__ = "1.19.0"
 
 # Version and executable information
 EXE = "snippy-ng"
