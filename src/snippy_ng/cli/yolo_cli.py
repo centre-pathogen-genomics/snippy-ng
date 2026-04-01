8ee1d528ee73b94288a2c00880c744be00f774fc
Wytamma Wirth <wytamma.wirth@me.com>
2026-04-01 18:03:39 +1100

    feat: Enhance YOLO pipeline to accept reference argument and improve CPU allocation

--- src/snippy_ng/cli/yolo_cli.py
+++ src/snippy_ng/cli/yolo_cli.py
@@ -2,25 +2,29 @@
 Thanks for looking at the source code! You found a hidden command :D
 """
 
+import os
+
 from pathlib import Path
 from typing import Any, Iterable, List
 import click
 
 from snippy_ng.cli.utils import AbsolutePath
-from snippy_ng.cli.utils.globals import CommandWithGlobals, add_snippy_global_options
+from snippy_ng.cli.utils.globals import CommandWithGlobals, GlobalOption, add_snippy_global_options
 
 
 @click.command(
     cls=CommandWithGlobals, context_settings={"show_default": True}, hidden=True
 )
-@add_snippy_global_options()
+@click.option("--cpus", "-c", default=None, required=False, type=int, help="Maximum number of CPUs to use. By default, uses all available CPUs.", cls=GlobalOption)
+@add_snippy_global_options(['cpus'])
+@click.option("--reference", "--ref", required=False, type=AbsolutePath(exists=True, readable=True), help="Reference genome (FASTA or GenBank) or prepared reference directory")
 @click.argument(
     "directory",
     required=False,
     type=AbsolutePath(exists=True, readable=True),
     nargs=-1
 )
-def yolo(directory: Iterable[Path], outdir: Path, prefix: str, **context: Any):
+def yolo(directory: Iterable[Path], reference: Path, outdir: Path, prefix: str, **context: Any):
     """
     Pipeline that automates everything.
 
@@ -58,22 +62,22 @@ def yolo(directory: Iterable[Path], outdir: Path, prefix: str, **context: Any):
 
     # find reference and reads in the directories
     # look for file called reference or ref with fasta, fa, fna, gbk, genbank extension
-    reference = None
-    for search_dir in directories:
-        for ext in ["fasta", "fa", "fna", "gbk", "genbank"]:
-            for name in ["reference", "ref"]:
-                candidates = list(search_dir.rglob(f"{name}.{ext}"))
-                if candidates:
-                    reference = candidates[0].absolute()
-                    logger.info(f"Found reference file: {reference}")
+    if reference is None:
+        for search_dir in directories:
+            for ext in ["fasta", "fa", "fna", "gbk", "genbank"]:
+                for name in ["reference", "ref"]:
+                    candidates = list(search_dir.rglob(f"{name}.{ext}"))
+                    if candidates:
+                        reference = candidates[0].absolute()
+                        logger.info(f"Found reference file: {reference}")
+                        break
+                if reference:
                     break
             if reference:
                 break
-        if reference:
-            break
     if not reference:
         raise InvalidReferenceError(
-            "No reference file found! Please ensure you have a file called `reference` with one of the following extensions: fasta, fa, fna, gbk, genbank e.g. reference.fasta or ref.gbk"
+            "No reference file found! Please proved `--reference` or ensure you have a file called `reference` with one of the following extensions: fasta, fa, fna, gbk, genbank e.g. reference.fasta or ref.gbk"
         )
 
     # find all samples in the directory and create config
@@ -103,13 +107,15 @@ def yolo(directory: Iterable[Path], outdir: Path, prefix: str, **context: Any):
         output_directory=Path(outdir) / "reference",
     )
     ref_pipeline = SnippyPipeline(stages=[ref_stage])
+    if context["cpus"] is None:
+        context["cpus"] = os.cpu_count() or 1
     context["outdir"] = outdir
     run_ctx = Context(**context)
     ref_pipeline.run(run_ctx)
 
     snippy_reference_dir = ref_stage.output.reference.parent
 
-    # each sample gets 2 CPUs or total_cpus / num_samples, whichever is higher
+    # each sample gets 4 CPUs or total_cpus / num_samples, whichever is higher
     cpus_per_sample = max(4, context["cpus"] // len(samples))
     try:
         run_multi_pipeline(
@@ -147,6 +153,8 @@ def yolo(directory: Iterable[Path], outdir: Path, prefix: str, **context: Any):
     # tree
     from snippy_ng.pipelines.tree import TreePipelineBuilder
 
+
+    context["ram"] = None # YOLO: disable RAM limiting for this step
     tree_pipeline = TreePipelineBuilder(
         aln=core_outdir / aln_pipeline.stages[-1].output.soft_core,
         fconst=(core_outdir / aln_pipeline.stages[-1].output.constant_sites).read_text().strip(),
@@ -155,4 +163,18 @@ def yolo(directory: Iterable[Path], outdir: Path, prefix: str, **context: Any):
     tree_outdir = Path(outdir) / "tree"
     context["outdir"] = tree_outdir
     tree_run_ctx = Context(**context)
-    return tree_pipeline.run(tree_run_ctx)
+    tree_pipeline.run(tree_run_ctx)
+
+    # report
+    from snippy_ng.pipelines.report import ReportPipelineBuilder
+
+    report_pipeline = ReportPipelineBuilder(
+        tree=tree_outdir / tree_pipeline.stages[-1].output.tree,
+        title="Snippy-NG Report",
+        metadata=Path(outdir) / "snippy.vcf.summary.tsv",
+        prefix="report",
+    ).build()
+    report_outdir = Path(outdir) / "report"
+    context["outdir"] = report_outdir
+    report_run_ctx = Context(**context)
+    return report_pipeline.run(report_run_ctx)
